0f5466acf8bfaa1404f7041315775be5da6ea9d5
SilverAndrels <andresilva64@outlook.com>
2022-08-01 22:33:05 -0300

    change alot of things

--- Excalibur.py
+++ Excalibur.py
@@ -1,14 +1,13 @@
 from os import system as sy
 while True:
-    sy("clear")
+    sy("cls")
     print("-" * 66)
-    print("Program made for help with some anotations by WhoamiSilver v.0.0.2")
+    print("Program made for help with some anotations by SilverAndrels v.0.0.2")
     print("-" * 66)
     anotations = input("Input your anotation: ")
     if anotations == "exit":
-        sy("clear")
+        sy("cls")
         break
-    else:
-        with open("ant.txt", "a") as file:
-            file.write(anotations + "\n")
-            sy("clear")
+    with open("ant.txt", "a") as file:
+        file.write(anotations + "\n")
+    sy("cls")
