feda1f9b1e3644c9daa16956c66db1e3a31da1db
SilverAndrels <andresilva64@outlook.com>
2022-08-04 13:14:44 -0300

    Version 0.0.3

--- Excalibur.py
+++ Excalibur.py
@@ -1,13 +1,19 @@
 from os import system as sy
+import pyautogui as py
 while True:
     sy("cls")
-    print("-" * 66)
-    print("Program made for help with some anotations by SilverAndrels v.0.0.2")
-    print("-" * 66)
-    anotations = input("Input your anotation: ")
+    print("-" * 67)
+    print("Program made for help with some anotations by SilverAndrels v.0.0.3")
+    print("-" * 67)
+    anotations = input("Input your anotation: ").lower()
     if anotations == "exit":
         sy("cls")
         break
-    with open("ant.txt", "a") as file:
-        file.write(anotations + "\n")
-    sy("cls")
+    if anotations == "read":
+        py.hotkey('win', 'r')
+        py.write('C:/ProgramData/dist/ant.txt')
+        py.press('enter')
+    else:
+        with open("C:/ProgramData/dist/ant.txt", "a") as file:
+            file.write(anotations + "\n")
+        sy("cls")
